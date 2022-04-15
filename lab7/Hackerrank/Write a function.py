bdfc74530258d495cbe6dcfdcc127091eeb93f78
LisMorozniy <78703274+LisMorozniy@users.noreply.github.com>
2022-04-15 08:53:19 +0600

    Delete lab7 directory

--- lab7/Hackerrank/Write a function.py
+++
@@ -1,9 +0,0 @@
-def is_leap(n):
-    leap = False
-    
-    if((((n % 4) == 0) and ((n % 100) != 0)) or (n % 400 == 0)):
-        leap = True
-    return leap
-
-year = int(input())
-print(is_leap(year))
--- lab7/Массивы/A.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-a = input().split()
-i = 0
-for y in a:
-    if i % 2 == 0:
-        print(y)
-    i+=1
